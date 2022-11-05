340ba4194830bd69e839880148d4a330f390fa5b
JaySant <jaysant15@gmail.com>
2022-11-05 01:02:12 -0300

    Requisito 4

--- src/routers/productsRouters.js
+++ src/routers/productsRouters.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/products.controllers');
+const { validateName } = require('../middlewares/validate');
 
 const router = express.Router();
 
-router.post('/', controller.createProduct);
+router.post('/', validateName, controller.createProduct);
 router.get('/', controller.productAll);
 router.get('/:id', controller.productById);
 
